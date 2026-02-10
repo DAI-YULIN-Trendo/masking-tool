279cab05b487eae90f9b721ae1ae1b212d865dde
戴 <dylin0811@gmail.com>
2026-02-10 13:13:39 +0900

    fix: update base path and add workflow permissions

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mask-tool/',
+  base: '/masking-tool/',
   server: {
     port: 5766,
     fs: {
